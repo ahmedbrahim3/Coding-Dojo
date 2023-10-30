585596cb167ce1e07675328bba73bd83c76aa5a7
ahmedbrahim3 <ahmedbrahim2944@gmail.com>
2023-10-30 13:48:14 +0100

    fizzBuzz

---
+++ WebFundamentals/algo/math.js
@@ -0,0 +1,11 @@
+function d6(){
+var roll=Math.random()
+roll=Math.floor(Math.random()*7)
+return roll
+}   
+///////////////////////:
+function randomFromArray(arr){
+    var random=Math.floor(Math.random()*arr.length)
+        var lifesAnswers=arr[random]
+    return lifesAnswers
+    }
