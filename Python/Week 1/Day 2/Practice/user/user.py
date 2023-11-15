b3b94144815d9c2f7c8b34c0fdcfd4a66fc41ab7
ahmedbrahim3 <ahmedbrahim2944@gmail.com>
2023-11-15 15:33:22 +0100

    chaning

--- Python/Week 1/Day 2/Practice/user/user.py
+++ Python/Week 1/Day 2/Practice/user/user.py
@@ -9,21 +9,27 @@ class user:
 
     def display_info(self):
         print(f"my name is {self.fname} my laqt name is {self.lname} my email is {self.email} my points are {self.gold_card_points} ")
+        return self
+
     def enroll(self):
         self.is_rewards_member=True
         self.gold_card_points= 500
         print(f"is_rewards_member is {self.is_rewards_member} and the points are {self.gold_card_points}")
+        return self
+
+
     def spend_points(self, amount):
         self.gold_card_points-=amount
         print(f"the points are {self.gold_card_points}")
+        return self
+
 
             
 
 user1=user ("ahmed","brahim","a@g.c",22)
 user2=user ("bbbb","brahim","a@g.c",22)
 user3=user ("aaa","brahim","a@g.c",22)
-print(user1.display_info())
-user1.enroll()
-user1.spend_points(100)
+user1.display_info().enroll().spend_points(50).display_info()
+user2.enroll().spend_points(80).display_info()
 
 
