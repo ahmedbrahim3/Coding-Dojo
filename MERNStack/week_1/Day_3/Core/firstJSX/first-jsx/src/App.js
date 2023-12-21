61b6e65cd24ea9858d37c684acc4575e1f175055
ahmedbrahim3 <ahmedbrahim2944@gmail.com>
2023-12-21 12:58:16 +0100

    w1d3

---
+++ MERNStack/week_1/Day_3/Core/firstJSX/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log("hello from react app")
+  return (
+    <fieldset>
+      <h1>Hello Dojo !</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mount Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dog</li>
+      </ul> 
+    </fieldset>
+  );      
+}
+
+export default App;
