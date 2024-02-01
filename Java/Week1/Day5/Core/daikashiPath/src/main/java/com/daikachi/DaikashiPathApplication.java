1a9d86df08518b44349a655600cc2ce753964442
ahmedbrahim3 <ahmedbrahim2944@gmail.com>
2024-02-01 15:20:58 +0100

    daikidhi path

---
+++ Java/Week1/Day5/Core/daikashiPath/src/main/java/com/daikachi/DaikashiPathApplication.java
@@ -0,0 +1,13 @@
+package com.daikachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikashiPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikashiPathApplication.class, args);
+	}
+
+}
